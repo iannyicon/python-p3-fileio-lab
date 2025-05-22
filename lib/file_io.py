1a2c489fcabf98e95495ea7a76aee51a2be63027
Iann <iconian4@gmail.com>
2025-05-22 16:18:23 +0300

    python

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,23 @@
 def write_file(file_name, file_content):
-    pass
+    file_path = str(file_name)
+    if not file_path.endswith('.txt'):
+        file_path += '.txt'
+
+    with open(file_path, 'w') as f:
+        f.write(file_content)    
 
 def append_file(file_name, append_content):
-    pass
+    file_path = str(file_name)
+    if not file_path.endswith('.txt'):
+        file_path += '.txt'
+
+    with open(file_path, 'a') as f:
+        f.write(append_content)
 
 def read_file(file_name):
-    pass
+    file_path = str(file_name)
+    if not file_path.endswith('.txt'):
+        file_path += '.txt'
+
+    with open(file_path, 'r') as f:
+        return f.read()
